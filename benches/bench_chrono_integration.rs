8d267ee2d3b946acd72b5143b20670857a8e1deb
Federico Dolce <psykopear@gmail.com>
2022-09-06 12:24:15 +0200

    Added some more benchmarks

--- benches/bench_chrono_integration.rs
+++ benches/bench_chrono_integration.rs
@@ -1,4 +1,4 @@
-use chrono::{Duration, NaiveDate};
+use chrono::{DateTime, Duration, NaiveDate, NaiveDateTime, NaiveTime, Utc, FixedOffset, TimeZone};
 use criterion::{criterion_group, criterion_main, Bencher, Criterion};
 
 use pyo3::prelude::*;
@@ -17,11 +17,45 @@ fn naive_date_to_py(b: &mut Bencher<'_>) {
     });
 }
 
+fn naive_time_to_py(b: &mut Bencher<'_>) {
+    Python::with_gil(|py| {
+        const LEN: u32 = 50_000;
+        b.iter(|| (0..LEN).map(|i| NaiveTime::from_num_seconds_from_midnight(i, 0).into_py(py)));
+    });
+}
+
+fn datetime_to_py(b: &mut Bencher<'_>) {
+    Python::with_gil(|py| {
+        const LEN: i64 = 50_000;
+        b.iter(|| {
+            (0..LEN).map(|i| {
+                DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(i, 0), Utc).into_py(py)
+            })
+        });
+    });
+}
+
+fn datetime_fixed_offset_to_py(b: &mut Bencher<'_>) {
+    Python::with_gil(|py| {
+        const LEN: i32 = 50_000;
+        b.iter(|| {
+            (0..LEN).map(|i| {
+                FixedOffset::east(i)
+                    .ymd(2000, 1, 1)
+                    .and_hms(0, 0, 0)
+                    .into_py(py)
+            })
+        });
+    });
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("duration_to_py", duration_to_py);
     c.bench_function("naive_date_to_py", naive_date_to_py);
+    c.bench_function("naive_time_to_py", naive_time_to_py);
+    c.bench_function("datetime_to_py", datetime_to_py);
+    c.bench_function("datetime_fixed_offset_to_py", datetime_fixed_offset_to_py);
 }
-
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
